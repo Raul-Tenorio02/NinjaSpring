f017c152b4113ccae5bebe428f11c8fc91c9dc1e
Raul Tenorio <raul.amaral2005@gmail.com>
2025-03-11 20:24:20 -0300

    first commit

---
+++ src/main/java/dev/raul/NinjaSpring/NinjaSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.raul.NinjaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/raul/NinjaSpring/NinjaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.raul.NinjaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
